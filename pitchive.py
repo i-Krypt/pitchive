76b7effc7975893246e98a232a70635d8f097464
i-Krypt <vamay505@gmail.com>
2019-09-18 15:56:56 +0300

    added a form model, login and registration templates,and a route for the forms

--- pitchive.py
+++ pitchive.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f7e5863a2ac81ff4b4a4eea178454687'
 
 
 #dummy data
@@ -27,6 +29,21 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+# RegistrationForm route
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+# LoginForm route  
+@app.route('/login')
+def login():
+    form = LoginForm
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
