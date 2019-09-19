2a44291a1b6dd4c3297c709236324cf47d031b19
i-Krypt <vamay505@gmail.com>
2019-09-19 18:04:02 +0300

    added sqlite db to the project

--- pitchive.py
+++ pitchive.py
@@ -1,8 +1,33 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f7e5863a2ac81ff4b4a4eea178454687'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    pitch = db.relationship('Pitch', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+class Pitch(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Pitch('{self.title}', '{self.date_posted}')"
 
 
 #dummy data
